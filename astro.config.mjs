3c45cb722566950bf02e269ca26193092f014379
yusufciftci <yusufciftci@outlook.com.tr>
2024-04-10 12:24:20 +0300

    add unpic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { imageService } from "@unpic/astro/service";
 
 // https://astro.build/config
 export default defineConfig({
