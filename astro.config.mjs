11e4daa957d6eff80f2d2b1cf88b66d10a4c9db7
yusufciftci <yusufciftci@outlook.com.tr>
2024-04-10 12:11:21 +0300

    remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://ipekunuc.com",
