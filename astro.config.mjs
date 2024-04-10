7185ec91473dc9b8d6bd5fdbba8d7da4daf05703
yusufciftci <yusufciftci@outlook.com.tr>
2024-04-10 12:28:20 +0300

    remove unpic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { imageService } from "@unpic/astro/service";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ipekunuc.com",
   integrations: [mdx(), sitemap(), tailwind()],
-  image: {
-    service: imageService({
-      // This can usually be auto-detected
-      fallbackService: "netlify",
-      placeholder: "blurhash",
-      // This is the default
-      layout: "constrained",
-    }),
-  },
 });
