aee3f6882f4f5d1188a3a7a8adbc08ec616388ee
PlainMevi <132750983+PlainMevi@users.noreply.github.com>
2024-04-11 00:36:02 +0300

    url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ipekunuc.com",
+  site: "https://ipekunuc.netlify.app",
   integrations: [mdx(), sitemap(), tailwind()],
 });
