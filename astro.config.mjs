a3711ff4c14395f44f4444133536702f1f265b7e
ipekunu <iunuc@cxuni.com>
2025-07-11 19:50:48 +0300

    add snake

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
@@ -7,4 +7,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://ipekunuc.netlify.app",
   integrations: [mdx(), sitemap(), tailwind()],
+   image: {
+   service: passthroughImageService(),
+ },
 });
